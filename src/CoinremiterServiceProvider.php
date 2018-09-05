aca003955938a64ea6f1ef0333d3acc4f0b7f20d
CoinRemitter <42885529+CoinRemitter@users.noreply.github.com>
2018-09-05 10:18:31 +0530

    Create CoinremiterServiceProvider.php

---
+++ src/CoinremiterServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Coinremitterlaravel\CoinremitterLaravel;
+
+use Illuminate\Support\ServiceProvider;
+
+class CoinremiterServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+        
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        
+        //
+        $this->mergeConfigFrom(__DIR__.'/config/coinremitter.php', 'coinremitter');
+        $this->publishes([
+            __DIR__.'/config' => base_path('config'),
+        ]);
+
+    }
+}
