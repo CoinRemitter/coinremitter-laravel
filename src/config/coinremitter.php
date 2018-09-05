be7f3a23f0cba3b99e64409f13e65349ac4c2578
CoinRemitter <42885529+CoinRemitter@users.noreply.github.com>
2018-09-05 10:20:09 +0530

    Create coinremitter.php

---
+++ src/config/coinremitter.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'BTC'=>[
+        'API_KEY'=>'API_KEY_FROM_WEBSITE',
+        'PASSWORD'=>'PASSWORD',
+    ],
+    'LTC'=>[
+        'API_KEY'=>'API_KEY_FROM_WEBSITE',
+        'PASSWORD'=>'PASSWORD',
+    ],
+];
